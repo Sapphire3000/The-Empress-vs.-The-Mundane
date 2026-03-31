6b1a45b8ff7e4519b1c08c3e0c8e9194f19cb1ba
Sapphire3000 <159028778+Sapphire3000@users.noreply.github.com>
2026-03-31 05:06:42 -0400

    Delete saturnreturn.js

--- saturnreturn.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const nav_links = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click',() => {
-	nav_links.classList.toggle ('nav-active'); 
-});
